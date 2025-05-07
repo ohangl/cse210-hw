936a649f1878bac450bd92353b2123c1197a46e9
ohangl <angelachacongonzalez@gmail.com>
2025-05-07 15:27:23 -0300

    week 1-finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+       
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine("you guessed it! congrats!");
+            }
+
+        }                    
+    }  
 }
